47ab66278468339bf5d254d1ed0b634cacc10363
Elvarorn <elvar16@ru.is>
2019-09-25 13:28:15 +0000

    connection to database

--- data/db.js
+++ data/db.js
@@ -5,7 +5,12 @@ const auctionSchema = require('../schemas/auction');
 const auctionBidSchema = require('../schemas/auctionBid');
 const customerSchema = require('../schemas/customer');
 
-const connection = mongoose.createConnection('insert-your-mongodb-connection-string-here', { useNewUrlParser: true });
+const connection = mongoose.createConnection('mongodb+srv://Vefthjofar:MDSubastas@mansiondesubastasdb-jz5fd.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, 
+(err,client) => {
+    if(err) {throw new Error(err);}
+
+    console.log('Succsessfully connected to the Database');
+});
 
 module.exports = {
     Art: connection.model('Art', artSchema),
