11af0c6fcf2b07137d977d9ad27890868196a7a1
Grétar Hjartarson <gretarh17@ru.is>
2019-09-25 18:13:33 +0000

    get customer action bids komið

--- index.js
+++ index.js
@@ -11,86 +11,86 @@ app.use(bodyParser.json());
 
 // Art
 app.get("/api/arts", async function (req, res) {
-    const result = await artService.getAllArts();
-    return res.json(result);
+  const result = await artService.getAllArts();
+  return res.json(result);
 });
 
 app.get("/api/arts/:id", async function (req, res) {
-    const result = await artService.getArtById(req.params.id);
-    return res.json(result);
+  const result = await artService.getArtById(req.params.id);
+  return res.json(result);
 });
 
 app.post("/api/arts", function (req, res) {
-    artService.createArt(req.body, function (art) {
-        return res.status(201).json(art);
-    }, function (err) {
-        return res.status(400).json(err);
-    });
+  artService.createArt(req.body, function (art) {
+    return res.status(201).json(art);
+  }, function (err) {
+    return res.status(400).json(err);
+  });
 });
 
 // Artists
 app.get("/api/artists", async function (req, res) {
-    const result = await artistService.getAllArtists();
-    return res.json(result);
+  const result = await artistService.getAllArtists();
+  return res.json(result);
 });
 
 app.get("/api/artists/:id", async function (req, res) {
-    const result = await artistService.getArtistById(req.params.id);
-    return res.json(result);
+  const result = await artistService.getArtistById(req.params.id);
+  return res.json(result);
 });
 
 app.post("/api/artists", function (req, res) {
-    artistService.createArtist(req.body, function (artist) {
-        return res.status(201).json(artist);
-    }, function (err) {
-        return res.status(400).json(err);
-    });
+  artistService.createArtist(req.body, function (artist) {
+    return res.status(201).json(artist);
+  }, function (err) {
+    return res.status(400).json(err);
+  });
 });
 
 //Customers
 app.get("/api/customers", async function (req, res) {
-    const result = await customerService.getAllCustomers();
-    return res.json(result);
+  const result = await customerService.getAllCustomers();
+  return res.json(result);
 });
 
 app.get("/api/customers/:id", async function (req, res) {
-    const result = await customerService.getCustomerById(req.params.id);
-    return res.json(result);
+  const result = await customerService.getCustomerById(req.params.id);
+  return res.json(result);
 });
 
 app.post("/api/customers", function (req, res) {
-    customerService.createCustomer(req.body, function (customer) {
-        return res.status(201).json(customer);
-    }, function (err) {
-        return res.status(400).json(err);
-    });
+  customerService.createCustomer(req.body, function (customer) {
+    return res.status(201).json(customer);
+  }, function (err) {
+    return res.status(400).json(err);
+  });
 });
 
 app.get("/api/customers/:id/auction-bids", async function (req, res) {
-    const result = await customerService.getCustomerAuctionBids(req.params.id);
-    return res.json(result);
+  const result = await customerService.getCustomerAuctionBids(req.params.id);
+  return res.json(result);
 });
 
 // Auctions
 app.get("/api/auctions", async function (req, res) {
-    const result = await auctionService.getAllAuctions();
-    return res.json(result);
+  const result = await auctionService.getAllAuctions();
+  return res.json(result);
 });
 
 app.get("/api/auctions/:id", async function (req, res) {
-    const result = await auctionService.getAuctionById(req.params.id);
-    return res.json(result);
+  const result = await auctionService.getAuctionById(req.params.id);
+  return res.json(result);
 });
 
 app.post("/api/auctions", function (req, res) {
-    auctionService.createAuction(req.body, function (auction) {
-        return res.status(201).json(auction);
-    }, function (err) {
-        return res.status(400).json(err);
-    });
+  auctionService.createAuction(req.body, function (auction) {
+    return res.status(201).json(auction);
+  }, function (err) {
+    return res.status(400).json(err);
+  });
 });
 
 
 app.listen(3000, function () {
-    console.log("Server is listening on port 3000");
+  console.log("Server is listening on port 3000");
 });
--- services/customerService.js
+++ services/customerService.js
@@ -1,49 +1,49 @@
 const dbProvider = require("../data/db");
 
 const globalTryCatch = async cb => {
-  try {
-    return await cb();
-  } catch (err) {
-    return err;
-  }
+    try {
+        return await cb();
+    } catch (err) {
+        return err;
+    }
 }
 
 const customerService = () => {
-  const getAllCustomers = async (cb, errorCb) => {
-    return await globalTryCatch(async () => {
-      const customers = await dbProvider.Customer.find({});
-      return customers;
-    });
-  };
+    const getAllCustomers = async (cb, errorCb) => {
+        return await globalTryCatch(async () => {
+            const customers = await dbProvider.Customer.find({});
+            return customers;
+        });
+    };
 
-  const getCustomerById = async (id, cb, errorCb) => {
-    return await globalTryCatch(async () => {
-      const customer = await dbProvider.Customer.findById(id);
-      return customer;
-    });
-  };
+    const getCustomerById = async (id, cb, errorCb) => {
+        return await globalTryCatch(async () => {
+            const customer = await dbProvider.Customer.findById(id);
+            return customer;
+        });
+    };
 
     const getCustomerAuctionBids = async (Id, cb, errorCb) => {
         return await globalTryCatch(async () => {
-            const bids = await dbProvider.AuctionBid.find({customerId: Id});
+            const bids = await dbProvider.AuctionBid.find({ customerId: Id });
             return bids;
-          });
+        });
     };
 
-  const createCustomer = (customer, cb, errorCb) => {
-    // Your implementation goes here
-    const resp = dbProvider.Customer.create(customer, function (err, result) {
-      if (err) { errorCb(err); }
-      else { cb(result); }
-    });
-  };
+    const createCustomer = (customer, cb, errorCb) => {
+        // Your implementation goes here
+        const resp = dbProvider.Customer.create(customer, function (err, result) {
+            if (err) { errorCb(err); }
+            else { cb(result); }
+        });
+    };
 
-  return {
-    getAllCustomers,
-    getCustomerById,
-    getCustomerAuctionBids,
-    createCustomer
-  };
+    return {
+        getAllCustomers,
+        getCustomerById,
+        getCustomerAuctionBids,
+        createCustomer
+    };
 };
 
 module.exports = customerService();
